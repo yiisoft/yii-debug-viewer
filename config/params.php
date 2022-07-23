e0f7ac6d6a082da1389ff8e9d525bc7fc08b6627
Sergey <SergeySm128@gmail.com>
2022-07-24 00:11:03 +0300

    add validators

--- config/params.php
+++ config/params.php
@@ -2,28 +2,32 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Yii\Debug\Viewer\Panels\EventsPanel;
-use Yiisoft\Yii\Debug\Viewer\Panels\InfoPanel;
-use Yiisoft\Yii\Debug\Viewer\Panels\LogsPanel;
-use Yiisoft\Yii\Debug\Viewer\Panels\MiddlewaresPanel;
-use Yiisoft\Yii\Debug\Viewer\Panels\RequestPanel;
-use Yiisoft\Yii\Debug\Viewer\Panels\RoutesPanel;
-use Yiisoft\Yii\Debug\Viewer\Panels\ServicesPanel;
+use Yiisoft\Yii\Debug\Viewer\Panels\{
+    EventsPanel,
+    InfoPanel,
+    LogsPanel,
+    MiddlewaresPanel,
+    RequestPanel,
+    RoutesPanel,
+    ServicesPanel,
+    ValidatorsPanel
+};
 
 return [
     'yiisoft/yii-debug-viewer' => [
-        'enabled' => true,
-        'targetHost' => '/',
-        'apiUrl' => '/debug/api',
-        'viewerUrl' => '/debug',
+        'enabled'       => true,
+        'targetHost'    => '/',
+        'apiUrl'        => '/debug/api',
+        'viewerUrl'     => '/debug',
         'panels' => [
-            'info' => InfoPanel::class,
-            'request' => RequestPanel::class,
-            'routes' => RoutesPanel::class,
-            'logs' => LogsPanel::class,
-            'events' => EventsPanel::class,
-            'services' => ServicesPanel::class,
-            'middlewares' => MiddlewaresPanel::class,
+            'info'          => InfoPanel::class,
+            'request'       => RequestPanel::class,
+            'routes'        => RoutesPanel::class,
+            'logs'          => LogsPanel::class,
+            'events'        => EventsPanel::class,
+            'services'      => ServicesPanel::class,
+            'middlewares'   => MiddlewaresPanel::class,
+            'validators'    => ValidatorsPanel::class,
         ],
     ],
 ];
