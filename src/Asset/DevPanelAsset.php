bbfab69a0c0aa865e135592727ab71922b813bd7
Dmitrii Derepko <xepozz@list.ru>
2023-09-02 18:02:39 +0300

    Fix dev panel integration

--- src/Asset/DevPanelAsset.php
+++ src/Asset/DevPanelAsset.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Debug\Viewer\Asset;
 
 use Yiisoft\Assets\AssetBundle;
+use Yiisoft\View\WebView;
 
 final class DevPanelAsset extends AssetBundle
 {
     public bool $cdn = true;
 
     public array $js = [
-        'https://yiisoft.github.io/yii-dev-panel/bundle.js',
+        [
+            'https://yiisoft.github.io/yii-dev-panel/bundle.js',
+            WebView::POSITION_END,
+            'type' => 'module',
+        ],
+        'https://yiisoft.github.io/yii-dev-panel/registerSW.js',
     ];
 
     public array $css = [
