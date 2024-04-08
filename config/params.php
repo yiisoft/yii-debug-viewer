e4df556a4211453fd8c34cf5fa76ede265c13d84
Dmitrii Derepko <xepozz@list.ru>
2024-04-09 02:18:56 +0700

    Fix url

--- config/params.php
+++ config/params.php
@@ -8,7 +8,7 @@ return [
         'targetHost' => '/',
         'apiUrl' => '/debug/api',
         'viewerUrl' => '/debug',
-        'backendUrl' => '127.0.0.1:8090',
+        'backendUrl' => 'http://127.0.0.1:8090',
         'containerId' => 'yii-debug-toolbar',
         'editorUrl' => 'phpstorm://open?url=file://{file}&line={line}',
     ],
