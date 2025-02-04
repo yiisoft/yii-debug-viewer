88b2e3f976286d3ffc148dd19fc1be34264f6857
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-02-04 16:13:45 +0300

    Support Yii Assets 5 + Bump PHP to 8.1 (#73)

--- src/Asset/ToolbarAsset.php
+++ src/Asset/ToolbarAsset.php
@@ -5,14 +5,27 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Debug\Viewer\Asset;
 
 use Yiisoft\Assets\AssetBundle;
+use Yiisoft\Assets\AssetManager;
 use Yiisoft\View\WebView;
 
+/**
+ * @psalm-import-type CssFile from AssetManager
+ * @psalm-import-type JsFile from AssetManager
+ */
 final class ToolbarAsset extends AssetBundle
 {
     public bool $cdn = true;
+
+    /**
+     * @psalm-var array<array-key, string|JsFile>
+     */
     public array $js = [
         ['/bundle.js', WebView::POSITION_END, 'type' => 'module'],
     ];
+
+    /**
+     * @psalm-var array<array-key, string|CssFile>
+     */
     public array $css = [
         '/bundle.css',
     ];
